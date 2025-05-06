efdcc595c53fc3166ddb1e6bdc509edbaf4864c1
Muhammad Amin Boubaker <CodeIter@users.noreply.github.com>
2025-05-06 13:50:49 +0100

    delegate to the configuration in pyproject.toml from your setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="threads-cli",
-    version="1.1.2",
-    packages=["threads_cli"],
-    install_requires=["requests", "typer", "rich", "python-dotenv"],
-    entry_points={"console_scripts": ["threads-cli=threads_cli.cli:main"]}
-)
+setup()
