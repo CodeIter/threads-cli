2fba4f79d7e755a80b490f41998aa6bab7a39f34
Muhammad Amin Boubaker <CodeIter@users.noreply.github.com>
2025-05-07 19:08:05 +0100

    Load BASE_URL from env

--- src/env.py
+++ src/env.py
@@ -5,4 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN", "")
+BASE_URL = os.getenv("BASE_URL", "https://graph.threads.net/v1.0")
+
 DRAFTS_FILE = os.getenv("DRAFTS_FILE", "drafts.json")
